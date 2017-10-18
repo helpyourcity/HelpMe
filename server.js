5d1f0080fd4dddde5335fef38669ac42c8503ab0
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-10-17 21:06:38 -1000

    I just made it so that our server side for users (user.js) is now sending back just the user Id through oour post request.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
 const bp = require('body-parser');
