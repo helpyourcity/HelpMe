d1eee1a2094434499a0190a10349b6518fb77302
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-10-17 20:17:29 -1000

    just modified the posting for users route to make it so that it goes through redux and it is now hitting the server, we not need to make it so that the data we get back is just the user id.

--- src/actions/Users.js
+++ src/actions/Users.js
@@ -1,10 +1,23 @@
+import querystring from "querystring";
+import axios from 'axios';
+
 // EXPORTS
-export const SIGNIN_USER = 'SIGNIN_USER';
+export const ADD_USER  = 'ADD_USER';
 
 // ACTIONS
-export const signIn = (user) => {
-  return {
-    type: SIGNIN_USER,
-    user: user
-  };
-};
+
+export const addUser = user => {
+  console.log('my user', user)
+  return (dispatch) => {
+    axios.post("/api/user/new", querystring.stringify(user))
+    .then((users) => {
+      console.log('User data coming in from the actions', users)
+      dispatch({
+        type: ADD_USER,
+        users: users.data
+      })
+    })
+  }
+}
+
+//USER_CREATED 
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 // import logo from './logo.svg';
+import {connect} from 'react-redux'
+import {signIn} from '../../actions/Users.js'
 import './App.css';
+import SignUp from '../SignUp/'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-       test
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div>
+            <h1>Sign Up!</h1>
+            <SignUp/>
+        </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return{
+        users: state.users
+    }
+}
+const ConnectedApp = connect(mapStateToProps)(App)
+export default ConnectedApp;
