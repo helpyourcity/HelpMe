aa0f752b471ef5c0ba5c918db6a28dfa74a022f0
rosswu24 <wuyawchia24@gmail.com>
2017-10-21 19:27:29 -1000

    got rid of duplicat header

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -3,24 +3,9 @@ import "./Header.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Header extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		//Create account href is used for testing, to be deleted
-		return (
-			<div className="header">
-				<div className="header-title">Help Me!</div>
-				<div className="header-nav">
-					<Link to="/"> Home </Link> /
-					<Link to="/user/login"> Login </Link> /
-					<Link to="/create/account"> Create Account </Link> /
-					<Link to="/create/account/address"> Logout</Link>
-				</div>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     //Create account href is used for testing, to be deleted
@@ -36,7 +21,6 @@ class Header extends Component {
       </div>
     );
   }
-
 }
 
 export default Header;
