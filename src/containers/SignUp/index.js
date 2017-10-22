cdb600205aaecafde08d8e38cdab7d5cd21152df
rosswu24 <wuyawchia24@gmail.com>
2017-10-21 16:59:22 -1000

    add an email field

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -1,228 +1,128 @@
-import {connect} from 'react-redux';
-import React, {Component} from 'react';
-import {addUser, userLocation} from '../../actions/Users.js';
-import PasswordMask from 'react-password-mask';
-import NumberFormat from 'react-number-format';
+import { connect } from "react-redux";
+import React, { Component } from "react";
+import { addUser, userLocation } from "../../actions/Users.js";
+import PasswordMask from "react-password-mask";
+import NumberFormat from "react-number-format";
+import Header from "../Header";
+import { Redirect, withRouter } from "react-router";
 
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      redirectAddress: false
+    };
+  }
+  componentWillMount() {
+    //used to test out front end. need this to run react
+  }
 
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+  handleFirstName(e) {
+    this.setState({
+      first_name: e.target.value
+    });
+  }
+  handleLastName(e) {
+    this.setState({
+      last_name: e.target.value
+    });
+  }
 
+  handleEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
 
-class SignUp extends Component{
-	constructor (props){
-		super(props);
-		this.state = {
-			first_name: '',
-      last_name: '',
-      password: '',
-			house_number: '',
-			street: '',
-			unit: '',
-			apt_num: '',
-			city: '',
-			state: '',
-			zip_code: '',
-			phone: '',
-		};
-	}
-	componentWillMount(){
-		//used to test out front end. need this to run react
-	} 
+  handlePhone(e) {
+    this.setState({
+      phone: e.target.value
+    });
+  }
 
-	handleUserName(e){
-		this.setState({
-			userName: e.target.value
-		})
-	}
-	handlePassword(e){
-		this.setState({
-			password: e.target.value
-		})
-	}
-	handleFirstName(e){
-		this.setState({
-			first_name: e.target.value
-		})
-	}
-	handleLastName(e){
-		this.setState({
-			last_name: e.target.value
-		})
-	}
-	handleAge(e){
-		this.setState({
-			age: e.target.value
-		})
-	}
-	handleBirthDate(e){
-		this.setState({
-			birthDate: e.target.value
-		})
-	}
-	handleHouseNumber(e){
-		this.setState({
-			house_number: e.target.value
-		})
-	}
-	handleStreetAddress(e){
-		this.setState({
-			street: e.target.value
-		})
-	}
-	handleUnit(e){
-		this.setState({
-			unit: e.target.value
-		})
-	}
-	handleApt_Num(e){
-		this.setState({
-			apt_num: e.target.value
-		})
-	}
-	handleCity(e){
-		this.setState({
-			city: e.target.value
-		})
-	}
-	handleState(e){
-		this.setState({
-			state: e.target.value
-		})
-	}
-	handleZipcode(e){
-		this.setState({
-			zip_code: e.target.value
-		})
-	}
-	handleEmail(e){
-		this.setState({
-			email: e.target.value
-		})
-	}
-	handlePhone(e){
-		this.setState({
-			phone: e.target.value
-		})
-	}
+  redirectLocation() {
+    // need this function to redirect user page to address
+    this.setState({
+      redirectAddress: true
+    });
+  }
 
-	submitUser(evt){ //maybe take away submit function? 
-		evt.preventDefault();
-		console.log('userInput:',this.state)
-			let newUser = {
-				first_name: 'this.state.first_name',
-				last_name: 'this.state.last_name',
-				password: 'this.state.password',
-				phone: '8081234567',
-				active: 'true',
-			}
-			console.log('newcard:',newUser)
-			this.props.addUser(newUser);
-		}
+  submitUser(evt) {
+    //maybe take away submit function?
+    evt.preventDefault();
+    this.redirectLocation();
+    console.log("userInput:", this.state);
+    let newUser = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      password: this.state.password,
+      phone: this.state.phone,
+      active: true
+    };
+    console.log("newcard:", newUser);
+    this.props.addUser(newUser);
+  }
 
-	render (){
-		console.log("render: ", this.state);
-		return(
+  render() {
+    if (this.state.redirectAddress) {
+      return <Redirect to="/create/address" />;
+    }
+    console.log("User C", this.state);
+    return (
       <div>
-      
-			<form method = "post" action = "/api/user/new">
-			<h3>First Name</h3>
-				<input
-				type ="text"
-				placeholder ="John"
-				onChange={this.handleFirstName.bind(this)}
-				/>
-
-			<h3>Last Name</h3>
-				<input
-				type ="text"
-				placeholder ="Doe"
-				onChange={this.handleLastName.bind(this)}
+        <h1>User Info</h1>
+        <Header />
+        <h3>First Name</h3>
+        <input
+          type="text"
+          placeholder="John"
+          onChange={this.handleFirstName.bind(this)}
         />
-        
+        <h3>Last Name</h3>
+        <input
+          type="text"
+          placeholder="Doe"
+          onChange={this.handleLastName.bind(this)}
+        />
+        <h3>Email</h3>
+        <input
+          type="text"
+          placeholder="youremail@gmail.com"
+          onChange={this.handleEmail.bind(this)}
+        />
         <h3>Password</h3>
-				<PasswordMask
-				placeholder = "Enter Password"
-				value={this.state.password}
-				onChange={this.handlePassword.bind(this)}
-				useVendorStyles ={false}
+        <PasswordMask
+          placeholder="Enter Password"
+          value={this.state.password}
+          onChange={this.handlePassword.bind(this)}
+          useVendorStyles={false}
         />
-        
-			<h3>House Number</h3>
-				<input
-				type ="text"
-				placeholder ="1234"
-				onChange={this.handleHouseNumber.bind(this)}
-				/>
-
-				<h3>Street</h3>
-				<input
-				type ="text"
-				placeholder ="Melia Court"
-				onChange={this.handleStreetAddress.bind(this)}
-				/>
-
-				<h3>Unit</h3>
-				<input
-				type ="text"
-				placeholder ="4"
-				onChange={this.handleUnit.bind(this)}
-				/>
-
-				<h3>Apartment Number</h3>
-				<input
-				type ="number"
-				placeholder ="403"
-				onChange={this.handleApt_Num.bind(this)}
-				/>
-
-			<h3>City</h3>
-				<input
-				type ="text"
-				placeholder ="Honolulu"
-				onChange={this.handleCity.bind(this)}
-				/>
-
-			<h3>State</h3>
-				<input
-				type ="text"
-				placeholder ="HI"
-				maxLength = "2"
-				onChange={this.handleState.bind(this)}
-				/>
-
-			<h3>ZipCode</h3>
-				<NumberFormat format = "#####" 
-				placeholder ="96818"
-				onChange={this.handleZipcode.bind(this)}
-				/>
-
-			<h3>Phone Number</h3>
-				<NumberFormat format = "(###) ###-####" mask = "_"
-				placeholder ="808-422-2222"
-				onChange={this.handlePhone.bind(this)}
-				/>
-
-			<br />
-			
-			<button onClick = {this.submitUser.bind(this)}>Create Account</button>
-		</form>
-			</div>
-		)
-	}
+        <br />
+        <button onClick={this.submitUser.bind(this)}>Create Account</button>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-	return {
-		//whatever the action is 
-		addUser: (text) => {
-			dispatch(addUser(text))
-		  }
-		
-	}
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    //whatever the action is
+    addUser: text => {
+      dispatch(addUser(text));
+    }
+  };
+};
 
-const ConnectedSignUp = connect(
-	null,
-	mapDispatchToProps  
-)(SignUp)
+const ConnectedSignUp = connect(null, mapDispatchToProps)(SignUp);
 
-export default ConnectedSignUp;
+export default withRouter(ConnectedSignUp);
