171ce27b87dc0fdfefe5b486764b15d62ad8afa0
rosswu24 <wuyawchia24@gmail.com>
2017-10-21 16:57:06 -1000

    change to use 'Link', instead if href

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Link
-} from 'react-router-dom'        
-import ConnectedSignUp from '.././containers/SignUp/index'
-import ConnectedUserLocation from '.././containers/SignUp/location'
-import ConnectedMap from '.././containers/Map/index'
-import Header from '../containers/Header'
+  Link,
+  IndexRoute
+} from "react-router-dom";
 
-const ReactRouter = ()=>(
-  <Router> 
+// import ConnectedSignUp from '.././containers/SignUp/index'
+import SignUp from ".././containers/SignUp/index";
+import ConnectedMap from ".././containers/Map/index";
+import Header from "../containers/Header";
+import UserLocation from "../containers/SignUp/location";
+
+const ReactRouter = () => (
+  <Router>
     <div>
-      <Route exact path ="/" component = {Header}/>
-      <Route exact path = "/" component = {ConnectedMap}/>
-      <Route path = "/create/account" component = {ConnectedUserLocation}/>
+      <Route exact path="/" component={Header} />
+      <Route exact path="/" component={ConnectedMap} />
+      <Route path="/create/account" component={SignUp} />
+      <Route path="/create/address" component={UserLocation} />
     </div>
   </Router>
-)
+);
 
-
-export default ReactRouter; 
+export default ReactRouter;
