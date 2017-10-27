43e50893460a0ddd0e18fefe65d2bdc4cc0a864a
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-10-26 20:29:40 -1000

    just fixed the backend so that our routes are protected and have to have the token be checked, I also made it so the routes are properly sending the correct information.

--- routes/rescue.js
+++ routes/rescue.js
@@ -1,60 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const app = express();
 
-const Rescue = require('../models').Rescue
+const Rescue = require("../models").Rescue;
 
-router.post('/help_requests', function (req, res) {
-    console.log('are we posting resque??', req.body)
-    Rescue.create({
-            helper_id: req.body.helper_id,
-            helpee_id: req.body.helpee_id,
-            time_start: req.body.time_start,
-            time_closed: req.body.time_closed,
-            reason_ended: req.body.reason_ended,
-        })
-        .then((rescue) => {
-            return Rescue.findAll({
-                    'order': [
-                        ['time_start', 'DESC']
-                    ],
-                    limit: 1,
-                    where: {
-                        helper_id: req.params.helper_id
-                    }
-                })
-                .then((rescue) => {
-                    res.json(rescue);
-                });
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+const passportService = require("../services/passport.js");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignIn = passport.authenticate("local", { session: false });
+
+router.post("/help_requests", function(req, res) {
+  console.log("are we posting resque??", req.body);
+  Rescue.create({
+    helper_id: req.body.helper_id,
+    helpee_id: req.body.helpee_id,
+    time_start: req.body.time_start,
+    time_closed: req.body.time_closed,
+    reason_ended: req.body.reason_ended
+  })
+    .then(rescue => {
+      return Rescue.findAll({
+        order: [["time_start", "DESC"]],
+        limit: 1,
+        where: {
+          helper_id: req.params.helper_id
+        }
+      }).then(rescue => {
+        res.json(rescue);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
-
-router.put('/help_requests/:id', function (req, res) {
-    //this is to connect the helper to the helpee by rescue id
-    console.log("console logging", req.body)
-    var rescueId = parseInt(parseInt(req.params.id));
-    Rescue.update({
-      helper_id: req.body.helper_id,
-    }, {
+router.put("/help_requests/:id", function(req, res) {
+  //this is to connect the helper to the helpee by rescue id
+  console.log("console logging", req.body);
+  var rescueId = parseInt(parseInt(req.params.id));
+  Rescue.update(
+    {
+      helper_id: req.body.helper_id
+    },
+    {
       where: {
         id: req.params.id
       }
+    }
+  )
+    .then(data => {
+      console.log("success");
+      return Rescue.findById(data.id).then(rescue => {
+        res.json(rescue);
+      });
     })
-    .then((data) =>{
-      console.log('success');
-      return Rescue.findById(data.id)
-        .then((rescue) =>{
-          res.json(rescue);
-        });
-    })
-    .catch(() =>{
-      console.log('fail');
+    .catch(() => {
+      console.log("fail");
     });
-    });
-
+});
 
 module.exports = router;
